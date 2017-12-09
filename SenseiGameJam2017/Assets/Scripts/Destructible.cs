987bd5c96b7cbba4db8d927b846a0f618378335a
Dell-AdamPawlewski <adampawlewski@gmail.com>
2017-12-09 15:06:55 +0100

    Blood decal

---
+++ SenseiGameJam2017/Assets/Scripts/Destructible.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Destructible : MonoBehaviour {
+
+    void OnTriggerEnter(Collider col) {
+        if(col.tag == "Bullet") {
+            for(int i = 0; i < transform.childCount; i++) {
+                transform.GetChild(i).GetComponent<Rigidbody>().AddExplosionForce(150, col.transform.position, 5);
+                transform.GetChild(i).GetComponent<Rigidbody>().useGravity = true;
+            }
+        }
+    }
+}
