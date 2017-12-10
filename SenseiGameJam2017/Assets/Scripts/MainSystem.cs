f7ef2322d0ff40e2e50455422d14bcf1b9da5fc5
Dell-AdamPawlewski <adampawlewski@gmail.com>
2017-12-10 02:45:52 +0100

    random

--- SenseiGameJam2017/Assets/Scripts/MainSystem.cs
+++ SenseiGameJam2017/Assets/Scripts/MainSystem.cs
@@ -6,7 +6,7 @@ public class MainSystem : MonoBehaviour {
     public static float timer;
     public float TimeOfTurn;
     public GameObject playerPrefab;
-    public Transform spawnPoint;
+    public Transform[] spawnPoint;
     public static GameObject activePlayer;
 
     public List<TimeController> spawnedPlayers = new List<TimeController>();
@@ -37,7 +37,8 @@ public class MainSystem : MonoBehaviour {
 
             if (AND) {
                 timer = 0;
-                GameObject go = Instantiate(playerPrefab, spawnPoint);
+                GameObject go = Instantiate(playerPrefab, spawnPoint[Random.Range(0, spawnPoint.Length)].position, Quaternion.identity);
+                go.GetComponent<PlayerMovement>().weaponType = (WeaponType)Random.Range(0, 2);
                 spawnedPlayers.Add(go.GetComponent<TimeController>());
                 activePlayer = spawnedPlayers[spawnedPlayers.Count - 1].gameObject;
                 TimeController.rewinding = false;
